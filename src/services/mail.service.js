6ae37832eb174bcfffad205687985783be7c5f90
Harshil000 <techshock01@gmail.com>
2026-05-08 23:50:16 +0530

    set dns to solve production mail

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,13 +1,13 @@
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    family: 4,
+    service: 'gmail',
     auth: {
         type: 'OAuth2',
         user: process.env.GOOGLE_USER,
